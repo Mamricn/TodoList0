c45dfb87d4193bef648dcf62c7943cfd34e1562b
Mamricn <marcin.turek.27@wp.pl>
2025-04-18 21:03:51 +0100

    Initial Commit

---
+++ TodoList2/TodoList2App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList2App.swift
+//  TodoList2
+//
+//  Created by Marcin Turek on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
