e3ce1b7a6f5ee0734b10edc2a42e9914d1f57f64
Mamricn <marcin.turek.27@wp.pl>
2025-04-27 20:36:45 +0100

    [Feature] Add ProfileView

---
+++ TodoList2/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  TodoList2
+//
+//  Created by Marcin Turek on 27/04/2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hello, Marcin!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
